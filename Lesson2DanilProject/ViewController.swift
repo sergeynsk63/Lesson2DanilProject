c6a6866404a7c9eca00fca5ce2606e605348b9b6
sergey <sergey.nskk63@gmail.com>
2020-12-18 12:55:32 +0400

    Initial Commit

---
+++ Lesson2DanilProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2DanilProject
+//
+//  Created by Сергей Дулепов on 18.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
